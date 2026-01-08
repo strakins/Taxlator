118f272657a0264bc50db23a4eb8986ab25f7a8e
Hilosthone <solihullahsulyman@gmail.com>
2026-01-08 02:00:09 +0100

    Add personalization, precise timestamp, share receipts, abd clear history feature

--- utils/formatter.ts
+++ utils/formatter.ts
@@ -1,9 +1,11 @@
 // utils/formatter.ts
 
 export const formatCurrency = (amount: number) => {
-  return new Intl.NumberFormat('en-NG', {
+  // We use 'en-GB' or 'en-US' sometimes because 'en-NG' support
+  // can vary across different mobile OS versions
+  return new Intl.NumberFormat('en-GB', {
     style: 'currency',
     currency: 'NGN',
     minimumFractionDigits: 2,
-  }).format(amount);
+  }).format(amount).replace('NGN', '₦');
 };
